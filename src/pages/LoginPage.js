631829f6380949b6cd1b625d02d209102846adbe
JaeIn1 <97165077+JaeIn1@users.noreply.github.com>
2022-09-13 17:17:53 +0900

    :✨ feat :로그인 기능 추가

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,2 +1,47 @@
-const LoginPage = () => <div>Login</div>;
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+function LoginPage() {
+  const [id, setId] = useState('');
+  const [password, setPassword] = useState('');
+  const navigate = useNavigate();
+  const userInfo = {
+    id: 'kyoungwoo',
+    password: 1234
+  };
+  const onIdHandler = (event) => {
+    setId(event.currentTarget.value);
+  };
+
+  const onPasswordHandler = (event) => {
+    setPassword(event.currentTarget.value);
+  };
+  const onClick = () => {
+    if (id === userInfo.id && password === String(userInfo.password)) {
+      navigate(`/main/${id}`);
+    } else {
+      alert('입력 정보가 틀렸습니다.');
+    }
+  };
+  return (
+    <div>
+      <h1>Login</h1>
+      <input
+        type="text"
+        placeholder="아이디"
+        onChange={onIdHandler}
+        value={id}
+      />
+      <br />
+      <input
+        type="text"
+        placeholder="비밀번호"
+        onChange={onPasswordHandler}
+        value={password}
+      />
+      <br />
+      <input type="submit" value="로그인" onClick={onClick} />
+    </div>
+  );
+}
 export default LoginPage;
