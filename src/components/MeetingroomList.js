401f0764804024750488071258e334fc01e15d1e
Jangkyoungwoo <kyoungwoo990124@gmail.com>
2022-09-26 01:08:04 +0900

    ✨feat:  즐겨찾기 구별 로직 작성

--- src/components/MeetingroomList.js
+++ src/components/MeetingroomList.js
@@ -3,20 +3,45 @@ import { useEffect, useState } from 'react';
 
 function MeetingroomList() {
   const [meetingrooms, setMeetingrooms] = useState([]);
-  const [favorite, setFavorite] = useState([]);
+  const [notFav, setNotFav] = useState([]);
+  const [fav, setFav] = useState([]);
+  // const [toggle, setToggle] = useState(true);
 
-  const onFavorites = (event) => {
+  /* const onFavorites = (event) => {
     const eventId = Number(event.target.id);
-    // const meetingroomsId = meetingrooms[eventId - 1].id;
-    setFavorite(...meetingrooms[eventId - 1].name);
-    console.log(favorite);
-    // console.log(meetingrooms[eventId - 1].name);
+    if (meetingrooms[eventId - 1].isFav === true) {
+      setFavorite([...favorite, meetingrooms[eventId - 1].name]);
+      console.log(favorite);
+      setToggle(!toggle);
+    } else {
+      favorite.filter((fav) => fav.id !== eventId);
+      console.log(favorite);
+      setToggle(!toggle);
+    }
+  }; */
+
+  const isFavorite = () => {
+    const favArr = [];
+    const notFavArr = [];
+    for (let i = 0; i < meetingrooms.length; i++) {
+      if (meetingrooms[i].isFav === false) {
+        favArr.push(meetingrooms[i]);
+      } else {
+        notFavArr.push(meetingrooms[i]);
+      }
+    }
+    setFav(favArr);
+    setNotFav(notFavArr);
   };
 
   const getMeetingroom = async () => {
     try {
       const json = await axios.get('http://localhost:8000/data');
       setMeetingrooms(json.data.meetingrooms);
+      isFavorite();
+      console.log(fav);
+      console.log(notFav);
+      console.log(fav[0].name);
     } catch (error) {
       console.log(error);
     }
@@ -28,14 +53,25 @@ function MeetingroomList() {
   return (
     <div>
       <h5>자주 찾는 회의실</h5>
-      {meetingrooms.map((meetingroom) => (
+      {
+        fav.lentgh > 0? (fav.map((meetingroom) => (
         <div key={meetingroom.id}>
-          <button type="button" onClick={onFavorites} id={meetingroom.id}>
+          <button type="button" id={meetingroom.id}>
             ❤
           </button>
           <span>{meetingroom.name}</span>
         </div>
-      ))}
+      ))): (<hr />
+      notFav.map((meetingroom) => (
+        <div key={meetingroom.id}>
+          <button type="button" id={meetingroom.id}>
+            ❤
+          </button>
+          <span>{meetingroom.name}</span>
+        </div>
+      )))
+      }
+      
     </div>
   );
 }
