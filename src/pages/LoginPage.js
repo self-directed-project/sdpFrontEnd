6ce4476ac944743b7d4464e0b64dc3880f77b258
JaeIn1 <97165077+JaeIn1@users.noreply.github.com>
2022-09-28 19:15:57 +0900

    ✨ feat :세션 기능 추가

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,6 +1,6 @@
-import { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
+import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import loginLogo from '../img/loginScreen_logo.png';
 import './LoginPage.css';
 
@@ -8,23 +8,62 @@ function LoginPage() {
   const [id, setId] = useState('');
   const [password, setPassword] = useState('');
   const navigate = useNavigate();
-  const userInfo = {
-    id: 'ysy06053',
-    password: 1234
-  };
+
+  useEffect(() => {
+    const user = sessionStorage.getItem('user_id');
+    if (sessionStorage.getItem('user_id') === null) {
+      navigate('/');
+    } else {
+      navigate(`/main/${user}`);
+    }
+  }, []);
   const StartLogin = () => {
+    const setCookie = sessionStorage.getItem('user_id');
     const toData = {
-      userId: `${id}`,
-      userPw: `${password}`
+      username: `${id}`,
+      password: `${password}`
     };
     axios
       .post('http://localhost:5000/data', {
         headers: {
-          'Content-Type': 'application/json'
+          'Content-Type': 'application/json',
+          'Set-cookie': setCookie
         },
         data: toData
       })
-      .then(console.log);
+      .then((res) => {
+        console.log(res);
+        const userId = `${res.data.data.username}`;
+        const userPw = `${res.data.data.password}`;
+        console.log('res.data.userId = ', userId);
+        console.log('res.data.userPw = ', userPw);
+        if (userId === undefined) {
+          alert('입력하신 id 가 일치하지 않습니다.');
+        } else if (userId === null) {
+          // id는 있지만, pw 는 다른 경우 userId = null , msg = undefined
+          console.log('입력하신 비밀번호 가 일치하지 않습니다.');
+          alert('입력하신 비밀번호 가 일치하지 않습니다.');
+        } else if (userId === id) {
+          // id, pw 모두 일치 userId = userId1, msg = undefined
+          console.log('로그인 성공');
+          navigate(`/main/${id}`);
+          sessionStorage.setItem('user_id', id);
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+        const { config } = err;
+        const { status } = err.request;
+        if (err.request) {
+          // 요청이 이루어졌고 서버가 응답했을 경우
+          if (status === 404) {
+            console.log(`${config.url} not found`);
+          }
+          if (status === 500) {
+            console.log('Server error');
+          }
+        }
+      });
   };
   const onIdHandler = (event) => {
     setId(event.currentTarget.value);
@@ -33,14 +72,11 @@ function LoginPage() {
   const onPasswordHandler = (event) => {
     setPassword(event.currentTarget.value);
   };
+
   const onClick = () => {
-    if (id === userInfo.id && password === String(userInfo.password)) {
-      StartLogin();
-      navigate(`/main/${id}`);
-    } else {
-      alert('입력 정보가 틀렸습니다...');
-    }
+    StartLogin();
   };
+
   return (
     <div className="login">
       <header className="login-header">
--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -1,2 +1,8 @@
-const MainPage = () => <div>Main</div>;
+function MainPage() {
+  return (
+    <div>
+      <h5>자주 찾는 회의실</h5>
+    </div>
+  );
+}
 export default MainPage;
